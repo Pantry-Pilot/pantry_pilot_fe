72bb2eba494e06a3f2b7ed988f0360da65283394
Jorja Fleming <aset284@gmail.com>
2023-11-01 16:20:17 -0600

    Feat: Completed adding bootstrap and styling the log in page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,13 +7,12 @@ RSpec.describe "Welcome Page" do
         visit "/"
 
         expect(page).to have_content("Pantry Pilot")
-        expect(page).to have_xpath("//img[@src='https://www.realsimple.com/thmb/TQPhL9GrDM4-LuTBQQPSq9d7oyo=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/how-to-organize-pantry-e6f98662eed64700a7a39c11fec696c8.jpg']")
         expect(page).to have_button("Create an Account")
       end
 
       it "I see a navigation bar with a button to login" do
         visit "/"
-        save_and_open_page
+        # save_and_open_page
         expect(page).to have_button("Login")
       end
     end
