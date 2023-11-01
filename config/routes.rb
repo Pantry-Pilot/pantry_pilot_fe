a119627d07b38f1a31ab8f13599be71fd80dae5d
Lane Bretschneider <lanebret85@gmail.com>
2023-11-01 15:34:19 -0700

    refactor: modify name of controller from sessions to users for registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
 
+  get "/register", to: "users#registration_form"
+  post "/register", to: "users#register"
   get "/login", to: "sessions#login_form"
   post "/login", to: "sessions#login"
 end
