8971bfddf0e1c6139be794e923a672a11a7d4370
Lane Bretschneider <lanebret85@gmail.com>
2023-11-01 15:32:01 -0700

    feat: add controller with registration_form and register actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def registration_form
+
+  end
+
+  def register
+    if params[:password] == params[:confirm_password]
+      response = UsersFacade.register_user(params[:name], params[:email], params[:password])
+      if response[:status] == 201
+        redirect_to "/login"
+      else
+        flash[:error] = "Missing Required Information"
+        redirect_to "/register"
+      end
+    else
+      flash[:error] = "Passwords do not match"
+        redirect_to "/register"
+    end
+  end
+end
