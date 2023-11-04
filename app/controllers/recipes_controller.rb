d1b100675e3a88e3feed3d7339eda3d1c7cb53a2
Antoine Aube <aaube3@gmail.com>
2023-11-04 10:11:12 -0600

    feature: recipe create in controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,8 +10,20 @@ class RecipesController < ApplicationController
   end
 
   def show
-    # require 'pry';binding.pry
     recipe_data = RecipesService.new.find(params[:recipe_id])
     @recipe = Recipe.new(recipe_data[:data][:attributes])
   end
+
+  def create
+    recipe_data = params.permit(:recipe_id, :title, :image, :summary, :instructions, :ingredients)
+    recipe_data[:user_id] = session[:user_id] 
+    response = RecipesService.new.store_recipe(recipe_data)
+    # if response[:status] == 204
+    #   flash[:notice] = "Recipe added to your dashboard"
+    #   redirect_to "/dashboard"
+    # else 
+    #   flash[:error] = "Recipe already exists in your dashboard"
+    #   redirect_to "/dashboard"
+    # end
+  end 
 end
