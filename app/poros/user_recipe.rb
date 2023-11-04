6a4ee87eb06ed5563a4c29d3fa96c372ace1bb7e
Antoine Aube <aaube3@gmail.com>
2023-11-04 13:03:26 -0600

    User recipe poro to render back to dashboard

---
+++ app/poros/user_recipe.rb
@@ -0,0 +1,10 @@
+class UserRecipe
+  attr_reader :recipe_id, :title, :image
+
+  def initialize(data)
+    @id = data[:id]
+    @recipe_id = data[:attributes][:recipe_id]
+    @title = data[:attributes][:title]
+    @image = data[:attributes][:image]
+  end
+end
