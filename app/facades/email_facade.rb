387af8a03939d04c6a93de72b59e09e09d1e47d5
Antoine Aube <aaube3@gmail.com>
2023-11-06 09:25:07 -0700

    feature: error handling in email facade

--- app/facades/email_facade.rb
+++ app/facades/email_facade.rb
@@ -1,6 +1,7 @@
 class EmailFacade
   def email_recipe(recipient_email, recipe_id, sender_id)
     response = EmailService.new.send_recipe(recipient_email, recipe_id, sender_id)
+    # require 'pry';binding.pry
     {
       status: response[:status]
     }
