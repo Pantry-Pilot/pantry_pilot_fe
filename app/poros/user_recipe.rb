e9761dffa6a5e4f46ef542cb7cdd99e7a34e198a
Lane Bretschneider <lanebret85@gmail.com>
2023-11-06 16:41:31 -0800

    feat: add commented out code to abstract uploaded image into poro attribute

--- app/poros/user_recipe.rb
+++ app/poros/user_recipe.rb
@@ -1,10 +1,11 @@
 class UserRecipe
-  attr_reader :id, :recipe_id, :title, :image
+  attr_reader :id, :recipe_id, :title, :image#, :image_upload
 
   def initialize(data)
     @id = data[:id]
     @recipe_id = data[:attributes][:recipe_id]
     @title = data[:attributes][:title]
     @image = data[:attributes][:image]
+    # @image_upload = data[:attributes][:image_upload] || ""
   end
 end
