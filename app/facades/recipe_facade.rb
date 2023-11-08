c2688957b72c321461acb87e2e978a9fd7cb045c
Lane Bretschneider <lanebret85@gmail.com>
2023-11-08 07:46:09 -0800

    feat: add commented out code for method that was moved to avatars facade

--- app/facades/recipe_facade.rb
+++ app/facades/recipe_facade.rb
@@ -18,7 +18,7 @@ class RecipeFacade
 
   def store_recipe(data)
     response = RecipesService.new.store_recipe(data)
-    response_body = JSON.parse(response.body, symbolize_names: true)
+    # response_body = JSON.parse(response.body, symbolize_names: true)
 
     if response.status == 204
       {
@@ -48,21 +48,21 @@ class RecipeFacade
     end
   end
 
-  def add_image_to_recipe(recipe_data)
-    response = RecipesService.new.add_image_to_recipe(recipe_data)
-    response_body = JSON.parse(response.body, symbolize_names: true)
-    if response.status == 200
-      {
-        status: response.status,
-        notice: response_body[:notice]
-      }
-    else
-      {
-        status: response.status,
-        error: response_body[:error]
-      }
-    end
-  end
+  # def add_image_to_recipe(avatar_data)
+  #   response = RecipesService.new.add_image_to_recipe(avatar_data)
+  #   response_body = JSON.parse(response.body, symbolize_names: true)
+  #   if response.status == 200
+  #     {
+  #       status: response.status,
+  #       notice: response_body[:notice]
+  #     }
+  #   else
+  #     {
+  #       status: response.status,
+  #       error: response_body[:error]
+  #     }
+  #   end
+  # end
 
   private
 
