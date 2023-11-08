43197d44aac5131aa0f1e91c81eed533d2cb270e
Lane Bretschneider <lanebret85@gmail.com>
2023-11-08 14:02:02 -0800

    refactor: modify how a user_recipe object can accept an avatar url

--- app/poros/user_recipe.rb
+++ app/poros/user_recipe.rb
@@ -1,11 +1,12 @@
 class UserRecipe
-  attr_reader :id, :recipe_id, :title, :image#, :avatar
+  attr_reader :id, :recipe_id, :title, :image
+  attr_accessor :avatar
 
   def initialize(data)
     @id = data[:id]
     @recipe_id = data[:attributes][:recipe_id]
     @title = data[:attributes][:title]
     @image = data[:attributes][:image]
-    #@avatar = data[:attributes][:avatar]
+    @avatar = ""
   end
 end
