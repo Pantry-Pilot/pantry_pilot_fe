1b8f345d3b5ad201dedef3deb6e605ea624e9e18
Lane Bretschneider <lanebret85@gmail.com>
2023-11-08 14:03:06 -0800

    feat: add code to add an avatar url to the user_recipe object it corresponds with

--- app/facades/user_recipes_facade.rb
+++ app/facades/user_recipes_facade.rb
@@ -2,8 +2,13 @@ class UserRecipesFacade
   
   def get_user_recipes(user_id)
     response = UserRecipesService.new.get_user_recipes(user_id)
-    response[:data].map do |recipe_data|
+    recipes_array = response[:recipes][:data].map do |recipe_data|
       UserRecipe.new(recipe_data)
     end
+    avatars_array = response[:avatar_urls]
+    recipes_array.each_with_index do |recipe, index|
+      recipe.avatar = avatars_array[index] if avatars_array[index] != nil
+    end
+    recipes_array
   end
 end
