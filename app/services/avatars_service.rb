eb74af7226249ed5b19787c0c293dd829ff2153a
Lane Bretschneider <lanebret85@gmail.com>
2023-11-08 07:40:26 -0800

    feat: add service with conn private method and add_image_to_recipe method

---
+++ app/services/avatars_service.rb
@@ -0,0 +1,26 @@
+require 'faraday/multipart'
+
+class AvatarsService
+  def add_image_to_recipe(avatar_data)
+    payload = {
+      'recipe[id]': avatar_data[:id],
+      'recipe[avatar]': Faraday::UploadIO.new(avatar_data[:avatar].tempfile, avatar_data[:avatar].content_type, avatar_data[:avatar].original_filename)
+    }
+
+    conn.post("/api/v1/add_image", payload) do |req|
+      req.headers['Content-Type'] = 'multipart/form-data'
+      req.body = {id: avatar_data[:id], avatar: avatar_data[:avatar].tempfile}
+      # require 'pry';binding.pry
+    end
+  end
+  
+  private
+
+  def conn
+    Faraday.new(url: Rails.configuration.x.sessions_service_url) do |config|
+      config.request :multipart
+      config.request :url_encoded
+      config.adapter Faraday.default_adapter
+    end
+  end
+end
