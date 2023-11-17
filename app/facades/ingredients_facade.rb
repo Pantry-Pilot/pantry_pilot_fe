de6c414378f7b5eeded38ed41b5b2edcd3717c19
Antoine Aube <aaube3@gmail.com>
2023-11-16 17:54:16 -0700

    feature: user create and index facade

---
+++ app/facades/ingredients_facade.rb
@@ -0,0 +1,18 @@
+class IngredientsFacade
+  def self.create_user_ingredient(ingredient_data)
+    response = IngredientsService.new.add_user_ingredient(ingredient_data)
+    if response.status == 204
+      {status: response.status}
+    else
+      {status: response.status}
+    end
+  end
+
+  def self.get_user_ingredients(user)
+    response = IngredientsService.new.get_user_ingredients(user)
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    response_body[:ingredients][:data].map do |ingredient_data|
+      UserIngredient.new(ingredient_data)
+    end
+  end
+end
