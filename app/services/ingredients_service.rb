150c3529451b2ad9f2b2298c7478598f7a663fe5
Antoine Aube <aaube3@gmail.com>
2023-11-16 17:55:01 -0700

    feature: ingredients service calling to both the create and index endpoints of user ingredients

---
+++ app/services/ingredients_service.rb
@@ -0,0 +1,25 @@
+class IngredientsService
+
+  def add_user_ingredient(ingredient_data)
+    conn.post("/api/v1/add_ingredient") do |req|
+      req.body = ingredient_data.to_json
+    end
+  end
+
+  def get_user_ingredients(user)
+    conn.get("/api/v1/user_ingredients/#{user}")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: Rails.configuration.x.sessions_service_url) do |config|
+      config.request :json
+      config.adapter Faraday.default_adapter  
+    end
+  end
+
+  def parse_response(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
