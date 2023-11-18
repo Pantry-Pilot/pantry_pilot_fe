3ae514a22ad5aa16ace1a8c8ae68fb484bb159a9
Lane Bretschneider <lanebret85@gmail.com>
2023-11-17 19:02:31 -0800

    test: add test for search filter functionality

--- spec/features/recipes/index_spec.rb
+++ spec/features/recipes/index_spec.rb
@@ -46,4 +46,59 @@ RSpec.feature 'Recipes Index/Search', type: :feature do
     expect(page).to have_current_path("/dashboard/search", ignore_query: true)
     expect(page).to have_content('No recipes found')
   end
+
+  scenario 'User adds filters to their search', :vcr do
+    visit "/dashboard/search"
+
+    expect(page).to have_content("Filters")
+
+    expect(page).to have_content("Diet")
+    expect(page).to have_unchecked_field(:gluten_free)
+    expect(page).to have_unchecked_field(:ketogenic)
+    expect(page).to have_unchecked_field(:vegetarian)
+    expect(page).to have_unchecked_field(:lacto_vegetarian)
+    expect(page).to have_unchecked_field(:ovo_vegetarian)
+    expect(page).to have_unchecked_field(:vegan)
+    expect(page).to have_unchecked_field(:pescetarian)
+    expect(page).to have_unchecked_field(:paleo)
+    expect(page).to have_unchecked_field(:primal)
+    expect(page).to have_unchecked_field(:low_fodmap)
+    expect(page).to have_unchecked_field(:whole30)
+
+    expect(page).to have_content("Intolerances")
+    expect(page).to have_unchecked_field(:dairy)
+    expect(page).to have_unchecked_field(:egg)
+    expect(page).to have_unchecked_field(:gluten)
+    expect(page).to have_unchecked_field(:grain)
+    expect(page).to have_unchecked_field(:peanut)
+    expect(page).to have_unchecked_field(:seafood)
+    expect(page).to have_unchecked_field(:sesame)
+    expect(page).to have_unchecked_field(:shellfish)
+    expect(page).to have_unchecked_field(:soy)
+    expect(page).to have_unchecked_field(:sulfite)
+    expect(page).to have_unchecked_field(:tree_nut)
+    expect(page).to have_unchecked_field(:wheat)
+
+    expect(page).to have_content("Include Ingredient")
+    expect(page).to have_field(:include_ingredient)
+
+    expect(page).to have_content("Exclude Ingredient")
+    expect(page).to have_field(:exclude_ingredient)
+
+    fill_in :query, with: "steak"
+    check :dairy
+    check :seafood
+    click_on "Search"
+
+    expect(page).to have_current_path("/dashboard/search", ignore_query: true)
+
+    within '#recipes' do
+      expect(page).to have_css('.recipe-details', count: 10)
+
+      all('.recipe').each do |recipe|
+        expect(recipe.find('h2')).to have_link
+        expect(recipe).to have_css('img')
+      end
+    end
+  end
 end
