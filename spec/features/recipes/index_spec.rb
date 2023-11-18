fe56a6ea194f5e2d38d4077b71a586a6f9454e86
Lane Bretschneider <lanebret85@gmail.com>
2023-11-18 11:23:16 -0800

    fix: fix test for search filters to expect unchecked fields with the correct tag names

--- spec/features/recipes/index_spec.rb
+++ spec/features/recipes/index_spec.rb
@@ -27,7 +27,7 @@ RSpec.feature 'Recipes Index/Search', type: :feature do
 
     within '#recipes' do
       expect(page).to have_css('.recipe-details', count: 10)
-
+      
       all('.recipe').each do |recipe|
         expect(recipe.find('h2')).to have_link
         expect(recipe).to have_css('img')
@@ -37,61 +37,61 @@ RSpec.feature 'Recipes Index/Search', type: :feature do
 
   scenario 'User searches for a recipe with no results', :vcr do
     visit "/dashboard/search"
-
+    
     expect(page).to have_content('Search for recipes:')
-
+    
     fill_in 'query', with: 'asdfasdfasdfasdfasdfasdf'
     click_button 'Search'
-
+    
     expect(page).to have_current_path("/dashboard/search", ignore_query: true)
     expect(page).to have_content('No recipes found')
   end
 
   scenario 'User adds filters to their search', :vcr do
     visit "/dashboard/search"
-
+    
     expect(page).to have_content("Filters")
-
+    
     expect(page).to have_content("Diet")
-    expect(page).to have_unchecked_field(:gluten_free)
-    expect(page).to have_unchecked_field(:ketogenic)
-    expect(page).to have_unchecked_field(:vegetarian)
-    expect(page).to have_unchecked_field(:lacto_vegetarian)
-    expect(page).to have_unchecked_field(:ovo_vegetarian)
-    expect(page).to have_unchecked_field(:vegan)
-    expect(page).to have_unchecked_field(:pescetarian)
-    expect(page).to have_unchecked_field(:paleo)
-    expect(page).to have_unchecked_field(:primal)
-    expect(page).to have_unchecked_field(:low_fodmap)
-    expect(page).to have_unchecked_field(:whole30)
-
+    expect(page).to have_unchecked_field('diet[gluten free]')
+    expect(page).to have_unchecked_field('diet[ketogenic]')
+    expect(page).to have_unchecked_field('diet[vegetarian]')
+    expect(page).to have_unchecked_field('diet[lacto-vegetarian]')
+    expect(page).to have_unchecked_field('diet[ovo-vegetarian]')
+    expect(page).to have_unchecked_field('diet[vegan]')
+    expect(page).to have_unchecked_field('diet[pescetarian]')
+    expect(page).to have_unchecked_field('diet[paleo]')
+    expect(page).to have_unchecked_field('diet[primal]')
+    expect(page).to have_unchecked_field('diet[low FODMAP]')
+    expect(page).to have_unchecked_field('diet[whole30]')
+    
     expect(page).to have_content("Intolerances")
-    expect(page).to have_unchecked_field(:dairy)
-    expect(page).to have_unchecked_field(:egg)
-    expect(page).to have_unchecked_field(:gluten)
-    expect(page).to have_unchecked_field(:grain)
-    expect(page).to have_unchecked_field(:peanut)
-    expect(page).to have_unchecked_field(:seafood)
-    expect(page).to have_unchecked_field(:sesame)
-    expect(page).to have_unchecked_field(:shellfish)
-    expect(page).to have_unchecked_field(:soy)
-    expect(page).to have_unchecked_field(:sulfite)
-    expect(page).to have_unchecked_field(:tree_nut)
-    expect(page).to have_unchecked_field(:wheat)
-
-    expect(page).to have_content("Include Ingredient")
+    expect(page).to have_unchecked_field('intolerances[dairy]')
+    expect(page).to have_unchecked_field('intolerances[egg]')
+    expect(page).to have_unchecked_field('intolerances[gluten]')
+    expect(page).to have_unchecked_field('intolerances[grain]')
+    expect(page).to have_unchecked_field('intolerances[peanut]')
+    expect(page).to have_unchecked_field('intolerances[seafood]')
+    expect(page).to have_unchecked_field('intolerances[sesame]')
+    expect(page).to have_unchecked_field('intolerances[shellfish]')
+    expect(page).to have_unchecked_field('intolerances[soy]')
+    expect(page).to have_unchecked_field('intolerances[sulfite]')
+    expect(page).to have_unchecked_field('intolerances[tree_nut]')
+    expect(page).to have_unchecked_field('intolerances[wheat]')
+    
+    expect(page).to have_content("Include ingredient")
     expect(page).to have_field(:include_ingredient)
-
-    expect(page).to have_content("Exclude Ingredient")
+    
+    expect(page).to have_content("Exclude ingredient")
     expect(page).to have_field(:exclude_ingredient)
-
-    fill_in :query, with: "steak"
-    check :dairy
-    check :seafood
-    click_on "Search"
-
+    
+    fill_in 'query', with: 'chicken'
+    check 'intolerances[dairy]'
+    check'intolerances[seafood]'
+    click_button "Search"
+    
     expect(page).to have_current_path("/dashboard/search", ignore_query: true)
-
+    
     within '#recipes' do
       expect(page).to have_css('.recipe-details', count: 10)
 
